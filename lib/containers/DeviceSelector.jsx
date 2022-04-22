e982e775e592abde3333f70e38603d4686fe7798
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2022-04-22 08:58:49 +0200

    fix: use correct serialport casing for devicelisting
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

--- lib/containers/DeviceSelector.jsx
+++ lib/containers/DeviceSelector.jsx
@@ -11,7 +11,7 @@ import { DeviceSelector, logger } from 'pc-nrfconnect-shared';
 import * as ModemActions from '../actions/modemActions';
 
 const deviceListing = {
-    serialport: true,
+    serialPort: true,
 };
 
 export default () => {
